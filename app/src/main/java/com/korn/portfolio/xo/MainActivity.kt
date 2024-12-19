45def360c93a3b87dcf04fc800ff382bc8b032bd
Warakorn Mukdaprasert <warakorn.prasert@gmail.com>
2024-12-19 10:34:39 +0700

    First commit

---
+++ app/src/main/java/com/korn/portfolio/xo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.korn.portfolio.xo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.korn.portfolio.xo.ui.theme.XOTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            XOTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    XOTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/korn/portfolio/xo/ui/MainScreen.kt
@@ -0,0 +1,2 @@
+package com.korn.portfolio.xo.ui
+
