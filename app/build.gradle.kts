3c5a8490a08f4f64015fdcdbdf94cf0e7383f575
Warakorn Mukdaprasert <warakorn.prasert@gmail.com>
2024-12-22 18:50:45 +0700

    fix: splash screen makes action bar appear

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.material)
 
     api(libs.room.runtime)
     annotationProcessor(libs.room.compiler)
